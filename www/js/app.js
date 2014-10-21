6b3e132f4b6bd4bc8700f21be909e057aef61a2f
Holly Schinsky <hollyschinsky@gmail.com>
2014-10-20 23:44:31 -0400

    Initial checkin - complete solution

---
+++ www/js/app.js
@@ -0,0 +1,46 @@
+// We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
+(function () {
+
+    /* ---------------------------------- Local Variables ---------------------------------- */
+    HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
+    SessionListView.prototype.template = Handlebars.compile($("#session-list-tpl").html());
+    SessionView.prototype.template = Handlebars.compile($("#session-tpl").html());
+
+    var service = new ConferenceService();
+    var slider = new PageSlider($('body'));
+
+    service.initialize().done(function () {
+        router.addRoute('', function() {
+            console.log('empty');
+            slider.slidePage(new HomeView(service).render().$el);
+        });
+
+        router.addRoute('sessions/:id', function(id) {
+            console.log('details');
+            service.findById(parseInt(id)).done(function(session) {
+                slider.slidePage(new SessionView(session).render().$el);
+            });
+        });
+
+        router.start();
+    });
+
+    /* --------------------------------- Event Registration -------------------------------- */
+    document.addEventListener('deviceready', function () {
+        FastClick.attach(document.body);
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
+    }, false);
+
+    /* ---------------------------------- Local Functions ---------------------------------- */
+
+
+}());
